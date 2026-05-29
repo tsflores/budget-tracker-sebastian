7830d0e77dd00d74b44b046746672861f4e1abef
tsflores <trinidadfloresMS@outlook.com>
2026-05-29 12:44:34 -0400

    Budget Category schema

---
+++ server/models/BudgetCategory.ts
@@ -0,0 +1,31 @@
+import mongoose, { Document, Schema, Types } from "mongoose";
+
+export interface IBudgetCategory extends Document {
+  userId: Types.ObjectId;
+  name: string;
+  allocated: number;
+  color: string;
+  icon: string;
+}
+
+const BudgetCategorySchema = new Schema<IBudgetCategory>(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true,
+    },
+    name: { type: String, required: true },
+    allocated: { type: Number, required: true, defaul: 0 },
+    color: { type: String, required: true },
+    icon: { type: String, required: true },
+  },
+
+  { timestamps: false },
+);
+
+export default mongoose.model<IBudgetCategory>(
+  "BudgetCategory",
+  BudgetCategorySchema,
+);
